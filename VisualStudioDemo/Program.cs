f5ebf34c6a1f5b182245f3fe62c332fce6f31392
Rob Delgado <triptix@hotmail.com>
2018-11-04 19:42:56 -0600

    Modified message. Added ReadLine.

--- VisualStudioDemo/Program.cs
+++ VisualStudioDemo/Program.cs
@@ -6,7 +6,8 @@ namespace VisualStudioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 2");
+            Console.ReadLine();
         }
     }
 }
